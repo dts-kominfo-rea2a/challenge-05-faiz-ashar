df6a03f14968eff4717a5941b7fa669d9c409d68
ashar <faizashar6@gmail.com>
2022-06-05 21:47:04 +0800

    tugas 5

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (listNama, callbackSort)=>{
+  callbackSort(listNama);
+  let hasil = [];
+  for (let index = 0; index < listNama.length; index++) {
+    hasil.push(String(index + 1) +'. ' + listNama[index]);
+  }
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (mengurutkan)=>{
+  return mengurutkan.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (membalikkan)=>{
+  return membalikkan.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
